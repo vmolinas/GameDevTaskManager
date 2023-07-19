82f4afaac5eb34fef5a70c60744af6b39d542baa
vmolinas <u.vmolinas@gmail.com>
2023-07-19 20:56:30 -0300

    Primer commit. Archivos iniciales

---
+++ GameDevTaskManager/src/main/java/com/info2023/GameDevTaskManager/GameDevTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.info2023.GameDevTaskManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDevTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDevTaskManagerApplication.class, args);
+	}
+
+}
---
+++ GameDevTaskManager/src/test/java/com/info2023/GameDevTaskManager/GameDevTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info2023.GameDevTaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDevTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
